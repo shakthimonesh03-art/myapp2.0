aaf2de7d5046d67bf0ca1416aaffac3649f685ba
shakthimonesh03-art <shakthimonesh03@gmail.com>
2026-04-06 18:04:01 +0530

    Delete app directory

--- app/api/bookings/[id]/cancel/route.ts
+++
@@ -1,4 +0,0 @@
-import { POST as bookingsPost } from '@/app/api/bookings/route';
-export async function POST(_: Request, { params }: { params: { id: string } }) {
-  return bookingsPost(new Request('http://local/api/bookings', { method: 'POST', body: JSON.stringify({ action: 'cancel', bookingId: params.id }) }));
-}
