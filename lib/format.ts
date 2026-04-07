b023056b8cdbe0f2e4298b8e2199d0bac8dfafe8
shakthimonesh03-art <shakthimonesh03@gmail.com>
2026-04-07 15:22:56 +0530

    Delete lib directory

--- lib/format.ts
+++
@@ -1,7 +0,0 @@
-export function formatDateTime(value: string | number) {
-  return new Intl.DateTimeFormat('en-IN', {
-    dateStyle: 'medium',
-    timeStyle: 'short',
-    timeZone: 'Asia/Kolkata'
-  }).format(new Date(value));
-}
